20c090877535352252ab1b221f301e7343d4656b
Arkadiy <pv487732@gmail.com>
2024-12-19 12:57:09 +0300

    Controller Conpleted and tested

--- src/main/java/com/example/springpractice/controllers/AnimeController.java
+++ src/main/java/com/example/springpractice/controllers/AnimeController.java
@@ -2,9 +2,8 @@ package com.example.springpractice.controllers;
 
 import com.example.springpractice.AnimeService;
 import com.example.springpractice.models.Anime;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,30 @@ public class AnimeController {
     @GetMapping
     public List<Anime> getAllBooks() {
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public List<Anime> getAnimeById(@PathVariable int id) {
+        return service.findById(id).stream().toList();
+    }
+
+    @PostMapping
+    public Anime addAnime(@RequestBody Anime anime) {
+        return service.save(anime);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Anime> updateAnimeData(@PathVariable int id, @RequestBody Anime anime) {
+        return service.findById(id).map( el -> {
+            el.setTitle(anime.getTitle());
+            el.setCompany(anime.getCompany());
+            el.setDescription(anime.getDescription());
+            return ResponseEntity.ok(service.save(el));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAnime(@PathVariable int id) {
+        service.deleteById(id);
     }
 }
